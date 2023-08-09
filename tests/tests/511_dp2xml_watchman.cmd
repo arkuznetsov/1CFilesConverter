d6a8c939edcdad9693cc14ac460df12acb2fe14c
ArKuznetsov <ArKuznetsov@gmail.com>
2023-08-09 23:31:25 +0300

    test: Поправлена проверка успешности выполнения в тесте watchman.

--- tests/tests/511_dp2xml_watchman.cmd
+++ tests/tests/511_dp2xml_watchman.cmd
@@ -1,19 +1,19 @@
 @ECHO OFF
 
-SETLOCAL
-
 set TEST_NAME="Watchman trigger DP (binary folder) -> XML (using temp IB)"
 set TEST_OUT_PATH=%OUT_PATH%\%~n0
 set TEST_OUT_PATH=%TEST_OUT_PATH: =_%
-set TEST_CHECK_PATH=%TEST_OUT_PATH%\ВнешняяОбработка1.xml %TEST_OUT_PATH%\ВнешняяОбработка2.xml %TEST_OUT_PATH%\ВнешнийОтчет1.xml %TEST_OUT_PATH%\ВнешнийОтчет2.xml
+set TEST_CHECK_PATH=%TEST_OUT_PATH%\src\ext\ВнешняяОбработка1.xml %TEST_OUT_PATH%\src\ext\ВнешняяОбработка2.xml %TEST_OUT_PATH%\src\ext\ВнешнийОтчет1.xml %TEST_OUT_PATH%\src\ext\ВнешнийОтчет2.xml
 
 echo ===
 echo Test %TEST_COUNT%. ^(%~n0^) %TEST_NAME%
 echo ===
 
+SETLOCAL
+
 set V8_BASE_CONFIG="%FIXTURES_PATH%\bin\1cv8.cf"
 
-echo TEST_OUT_PATH: "%TEST_OUT_PATH%"
+md "%TEST_OUT_PATH%\src"
 md "%TEST_OUT_PATH%\ext"
 
 call %SCRIPTS_PATH%\..\wmscripts\settrigger.cmd "Test_dp2xml" "%TEST_OUT_PATH%" 1cdpr dp2xml "%TEST_OUT_PATH%\src"
