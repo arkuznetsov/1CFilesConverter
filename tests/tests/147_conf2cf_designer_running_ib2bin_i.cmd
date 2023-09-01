345458ad2300520b641a05b56d298bbed2df8390
Artem Kuznetsov <arkuznetsov@gmail.com>
2023-09-01 17:26:24 +0300

    test: Добавлены тесты конвертации конфигурации из серверной информационной базы при открытом конфигураторе.

---
+++ tests/tests/147_conf2cf_designer_running_ib2bin_i.cmd
@@ -0,0 +1,37 @@
+@ECHO OFF
+
+set TEST_NAME="Conf server infobase -> CF (ibcmd) with designer running"
+set TEST_OUT_PATH=%OUT_PATH%\%~n0\1cv8.cf
+set TEST_OUT_PATH=%TEST_OUT_PATH: =_%
+set TEST_CHECK_PATH=%TEST_OUT_PATH%
+set V8_PATH="C:\Program Files\1cv8\%V8_VERSION%\bin"
+set V8_CONVERT_TOOL=ibcmd
+
+echo ===
+echo Test %TEST_COUNT%. ^(%~n0^) %TEST_NAME%
+echo ===
+
+set "tasks_1c=tasklist /fi "imagename eq 1cv8.exe" /fo "list" | findstr "PID""
+for /f "tokens=2 delims==:" %%i in (' "%tasks_1c%" ') do (
+   if not defined pids_1c (
+      set pids_1c=%%i
+   ) else (
+      set pids_1c=!pids_1c!,%%i
+   )
+)
+set pids_1c=%pids_1c: =%
+
+start /D %V8_PATH% 1cv8.exe DESIGNER /IBConnectionString Srvr="%V8_SRV_ADDR%";Ref="%V8_IB_NAME%"; /DisableStartupDialogs
+timeout /T 5
+
+call %SCRIPTS_PATH%\conf2cf.cmd "/S%V8_SRV_ADDR%\%V8_IB_NAME%" "%TEST_OUT_PATH%"
+
+for /f "tokens=2 delims==:" %%i in (' "%tasks_1c%" ') do (
+   set cur_1c_pid=%%i
+   set cur_1c_pid=!cur_1c_pid: =!
+   set cur_1c_pid_isnew=1
+   for %%t in (%pids_1c%) do (
+       if "!pid!" equ "%%t" set cur_1c_pid_isnew=0
+   )
+   if "!cur_1c_pid_isnew!" equ "1" taskkill /PID !cur_1c_pid! /T /F
+)
