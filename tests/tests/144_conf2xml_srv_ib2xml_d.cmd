5446154f92fa6b2804cb065c7bedef2edf814ac5
Artem Kuznetsov <arkuznetsov@gmail.com>
2023-09-01 17:25:48 +0300

    test: Добавлены тесты конвертации конфигурации из серверной информационной базы.

---
+++ tests/tests/144_conf2xml_srv_ib2xml_d.cmd
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+set TEST_NAME="Conf server infobase -> XML (designer)"
+set TEST_OUT_PATH=%OUT_PATH%\%~n0
+set TEST_OUT_PATH=%TEST_OUT_PATH: =_%
+set TEST_CHECK_PATH=%TEST_OUT_PATH%\Configuration.xml
+set V8_CONVERT_TOOL=designer
+
+echo ===
+echo Test %TEST_COUNT%. ^(%~n0^) %TEST_NAME%
+echo ===
+call %SCRIPTS_PATH%\conf2xml.cmd "/S%V8_SRV_ADDR%\%V8_IB_NAME%" "%TEST_OUT_PATH%"
