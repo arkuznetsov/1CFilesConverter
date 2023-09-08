e61281a0765d245c2ec7de76ef2c932cd7d96e55
Artem Kuznetsov <arkuznetsov@gmail.com>
2023-09-08 07:55:08 +0300

    refactor: Изменена нумерация тестов; Поправлены заголовки тестов; Добавлен параметр создания информационной базы.

--- tests/tests/113_conf2ib_file_bin2ib_create_i.cmd
+++ tests/tests/113_conf2ib_file_bin2ib_create_i.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-set TEST_NAME="Conf XML -> infobase (ibcmd)"
+set TEST_NAME="Conf CF -> create file infobase (ibcmd)"
 set TEST_OUT_PATH=%OUT_PATH%\%~n0
 set TEST_OUT_PATH=%TEST_OUT_PATH: =_%
 set TEST_CHECK_PATH=%TEST_OUT_PATH%\1cv8.1cd
@@ -9,4 +9,5 @@ set V8_CONVERT_TOOL=ibcmd
 echo ===
 echo Test %TEST_COUNT%. ^(%~n0^) %TEST_NAME%
 echo ===
-call %SCRIPTS_PATH%\conf2ib.cmd "%TEST_XML_CF%" "%TEST_OUT_PATH%"
+call %SCRIPTS_PATH%\conf2ib.cmd "%TEST_BINARY%\1cv8.cf" "%TEST_OUT_PATH%" create
+
