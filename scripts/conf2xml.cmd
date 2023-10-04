8e48db48538c6c3871934ef0c90f99c149b468bd
Artem Kuznetsov <arkuznetsov@gmail.com>
2023-10-04 11:40:22 +0300

    fix: Убрана очистка целевого каталога при выгрузке в файлы.

--- scripts/conf2xml.cmd
+++ scripts/conf2xml.cmd
@@ -82,8 +82,7 @@ IF %ERROR_CODE% neq 0 (
 echo [INFO] Clear temporary files...
 IF exist "%LOCAL_TEMP%" rd /S /Q "%LOCAL_TEMP%"
 md "%LOCAL_TEMP%"
-IF exist "%V8_DST_PATH%" rd /S /Q "%V8_DST_PATH%"
-md "%V8_DST_PATH%"
+IF not exist "%V8_DST_PATH%" md "%V8_DST_PATH%"
 
 echo [INFO] Checking configuration source type...
 
