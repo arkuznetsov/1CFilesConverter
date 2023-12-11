ee22ae683aa69b5b1d476998abe71ce4483bba27
Artem Kuznetsov <ArKuznetsov@gmail.com>
2023-12-11 23:17:08 +0300

    feat: Реализован скрипт загрузки расширений в указанную информационную базу.

---
+++ scripts/ext2ib.cmd
@@ -0,0 +1,230 @@
+@rem ----------------------------------------------------------
+@rem This Source Code Form is subject to the terms of the
+@rem Mozilla Public License, v.2.0. If a copy of the MPL
+@rem was not distributed with this file, You can obtain one
+@rem at http://mozilla.org/MPL/2.0/.
+@rem ----------------------------------------------------------
+@rem Codebase: https://github.com/ArKuznetsov/1CFilesConverter/
+@rem ----------------------------------------------------------
+
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+IF not defined V8_ENCODING set V8_ENCODING=65001
+chcp %V8_ENCODING% > nul
+
+set CONVERT_VERSION=UNKNOWN
+IF exist "%~dp0..\VERSION" FOR /F "usebackq tokens=* delims=" %%i IN ("%~dp0..\VERSION") DO set CONVERT_VERSION=%%i
+echo 1C files converter v.%CONVERT_VERSION%
+echo ======
+echo [INFO] Convert 1C configuration extension to binary format ^(*.cfe^)
+
+set ERROR_CODE=0
+
+IF exist "%cd%\.env" (
+    FOR /F "usebackq tokens=*" %%a in ("%cd%\.env") DO (
+        FOR /F "tokens=1,2 delims==" %%b IN ("%%a") DO (
+            IF not defined %%b set "%%b=%%c"
+        )
+    )
+)
+
+IF not defined V8_VERSION set V8_VERSION=8.3.20.2290
+IF not defined V8_TEMP set V8_TEMP=%TEMP%\1c
+
+echo [INFO] Using 1C:Enterprise, version %V8_VERSION%
+echo [INFO] Using temporary folder "%V8_TEMP%"
+
+IF not "%V8_CONVERT_TOOL%" equ "designer" IF not "%V8_CONVERT_TOOL%" equ "ibcmd" set V8_CONVERT_TOOL=designer
+set V8_TOOL="C:\Program Files\1cv8\%V8_VERSION%\bin\1cv8.exe"
+IF "%V8_CONVERT_TOOL%" equ "designer" IF not exist %V8_TOOL% (
+    echo Could not find 1C:Designer with path %V8_TOOL%
+    set ERROR_CODE=1
+    goto finally
+)
+set IBCMD_TOOL="C:\Program Files\1cv8\%V8_VERSION%\bin\ibcmd.exe"
+IF "%V8_CONVERT_TOOL%" equ "ibcmd" IF not exist %IBCMD_TOOL% (
+    echo Could not find ibcmd tool with path %IBCMD_TOOL%
+    set ERROR_CODE=1
+    goto finally
+)
+
+IF defined V8_EDT_VERSION (
+    set V8_EDT_VERSION=@%V8_EDT_VERSION:@=%
+)
+
+echo [INFO] Start conversion using "%V8_CONVERT_TOOL%"
+
+set LOCAL_TEMP=%V8_TEMP%\%~n0
+if not defined IBCMD_DATA set IBCMD_DATA=%V8_TEMP%\ibcmd_data
+set IB_PATH=%LOCAL_TEMP%\tmp_db
+set XML_PATH=%LOCAL_TEMP%\tmp_xml
+set WS_PATH=%LOCAL_TEMP%\edt_ws
+
+set ARG=%1
+IF defined ARG set ARG=%ARG:"=%
+IF "%ARG%" neq "" set V8_SRC_PATH=%ARG%
+set ARG=%2
+IF defined ARG set ARG=%ARG:"=%
+IF "%ARG%" neq "" set V8_DST_PATH=%ARG%
+set V8_DST_FOLDER=%~dp2
+set V8_DST_FOLDER=%V8_DST_FOLDER:~0,-1%
+set ARG=%3
+IF defined ARG set ARG=%ARG:"=%
+IF "%ARG%" neq "" set V8_EXT_NAME=%ARG%
+
+IF not defined V8_SRC_PATH (
+    echo [ERROR] Missed parameter 1 - "path to 1C extension source (1C extension binary file (*.cfe), 1C:Designer XML files or 1C:EDT project)"
+    set ERROR_CODE=1
+) ELSE (
+    IF not exist "%V8_SRC_PATH%" (
+        echo [ERROR] Path "%V8_SRC_PATH%" doesn't exist ^(parameter 1^).
+        set ERROR_CODE=1
+    )
+)
+IF not defined V8_DST_PATH (
+    echo [ERROR] Missed parameter 2 - "path to 1C infobase"
+    set ERROR_CODE=1
+)
+IF not defined V8_EXT_NAME (
+    echo [ERROR] Missed parameter 3 - "configuration extension name"
+    set ERROR_CODE=1
+)
+IF %ERROR_CODE% neq 0 (
+    echo ======
+    echo [ERROR] Input parameters error. Expected:
+    echo     %%1 - path to 1C extension source ^(1C extension binary file ^(*.cfe^), 1C:Designer XML files or 1C:EDT project^)
+    echo     %%2 - path to 1C infobase
+    echo     %%3 - configuration extension name
+    echo.
+    goto finally
+)
+
+echo [INFO] Clear temporary files...
+IF exist "%LOCAL_TEMP%" rd /S /Q "%LOCAL_TEMP%"
+md "%LOCAL_TEMP%"
+IF not exist "%V8_DST_FOLDER%" md "%V8_DST_FOLDER%"
+
+echo [INFO] Checking extension %V8_DST_PATH% destination type...
+
+IF /i "%V8_DST_PATH:~0,2%" equ "/F" (
+    set IB_PATH=%V8_DST_PATH:~2%
+    echo [INFO] Destination type: File infobase ^(!IB_PATH!^)
+    set V8_IB_CONNECTION=File="!IB_PATH!";
+    goto check_src
+)
+IF /i "%V8_DST_PATH:~0,2%" equ "/S" (
+    set IB_PATH=%V8_DST_PATH:~2%
+    FOR /F "tokens=1,2 delims=\" %%a IN ("!IB_PATH!") DO (
+        set V8_IB_SERVER=%%a
+        set V8_IB_NAME=%%b
+    )
+    echo [INFO] Destination type: Server infobase ^(!V8_IB_SERVER!\!V8_IB_NAME!^)
+    set IB_PATH=!V8_IB_SERVER!\!V8_IB_NAME!
+    set V8_IB_CONNECTION=Srvr="!V8_IB_SERVER!";Ref="!V8_IB_NAME!";
+    IF not defined V8_DB_SRV_DBMS set V8_DB_SRV_DBMS=MSSQLServer
+    goto check_src
+)
+set IB_PATH=%V8_DST_PATH%
+IF exist "%IB_PATH%\1cv8.1cd" (
+    echo [INFO] Destination type: File infobase ^(%IB_PATH%^)
+    set V8_IB_CONNECTION=File="%IB_PATH%";
+    goto check_src
+)
+
+echo [ERROR] Error cheking type of destination "%V8_DST_PATH%"!
+echo Server or file infobase expected.
+    set ERROR_CODE=1
+    goto finally
+
+:check_src
+
+echo [INFO] Checking 1C extension source type...
+
+IF exist "%V8_SRC_PATH%\DT-INF\" (
+    IF exist "%V8_SRC_PATH%\src\Configuration\Configuration.mdo" (
+        FOR /F "delims=" %%t IN ('findstr /r /i "<objectBelonging>" "%V8_SRC_PATH%\src\Configuration\Configuration.mdo"') DO (
+            echo [INFO] Source type: 1C:EDT project
+            md "%XML_PATH%"
+            md "%WS_PATH%"
+            goto export_edt
+        )
+    )
+)
+IF exist "%V8_SRC_PATH%\Configuration.xml" (
+    FOR /F "delims=" %%t IN ('findstr /r /i "<objectBelonging>" "%V8_SRC_PATH%\Configuration.xml"') DO (
+        echo [INFO] Source type: 1C:Designer XML files
+        set XML_PATH=%V8_SRC_PATH%
+        goto export_xml
+    )
+)
+IF /i "%V8_SRC_PATH:~-4%" equ ".cfe" (
+    echo [INFO] Source type: Configuration extension file ^(CFE^)
+    goto export_cfe
+)
+
+echo [ERROR] Wrong path "%V8_SRC_PATH%"!
+echo Configuration extension binary ^(*.cfe^) or folder containing configuration extension in 1C:Designer XML format or 1C:EDT project expected.
+set ERROR_CODE=1
+goto finally
+
+:export_edt
+
+echo [INFO] Export configuration extension from 1C:EDT format "%V8_SRC_PATH%" to 1C:Designer XML format "%XML_PATH%"...
+IF not defined V8_RING_TOOL (
+    FOR /F "usebackq tokens=1 delims=" %%i IN (`where ring`) DO (
+        set V8_RING_TOOL="%%i"
+    )
+)
+IF not defined V8_RING_TOOL (
+    echo [ERROR] Can't find "ring" tool. Add path to "ring.bat" to "PATH" environment variable, or set "V8_RING_TOOL" variable with full specified path 
+    set ERROR_CODE=1
+    goto finally
+)
+call %V8_RING_TOOL% edt%V8_EDT_VERSION% workspace export --project "%V8_SRC_PATH%" --configuration-files "%XML_PATH%" --workspace-location "%WS_PATH%"
+IF not ERRORLEVEL 0 (
+    set ERROR_CODE=%ERRORLEVEL%
+    goto finally
+)
+
+:export_xml
+
+echo [INFO] Loading configuration extension from XML-files "%XML_PATH%" to infobase "%IB_PATH%"...
+IF "%V8_CONVERT_TOOL%" equ "designer" (
+    set V8_DESIGNER_LOG=%LOCAL_TEMP%\v8_designer_output.log
+    %V8_TOOL% DESIGNER /IBConnectionString %V8_IB_CONNECTION% /N"%V8_IB_USER%" /P"%V8_IB_PWD%" /DisableStartupDialogs /Out "!V8_DESIGNER_LOG!" /LoadConfigFromFiles "%XML_PATH%" -Extension %V8_EXT_NAME%
+    FOR /F "tokens=* delims=" %%i IN (!V8_DESIGNER_LOG!) DO IF "%%i" neq "" echo [WARN] %%i
+) ELSE (
+    IF defined V8_IB_SERVER (
+        %IBCMD_TOOL% infobase config import --data="%IBCMD_DATA%" --dbms=%V8_DB_SRV_DBMS% --db-server=%V8_IB_SERVER% --db-name="%V8_IB_NAME%" --db-user="%V8_DB_SRV_USR%" --db-pwd="%V8_DB_SRV_PWD%" --user="%V8_IB_USER%" --password="%V8_IB_PWD%" --extension=%V8_EXT_NAME% "%XML_PATH%"
+    ) ELSE (
+        %IBCMD_TOOL% infobase config import --data="%IBCMD_DATA%" --db-path="%IB_PATH%" --user="%V8_IB_USER%" --password="%V8_IB_PWD%" --extension=%V8_EXT_NAME% "%XML_PATH%"
+    )
+)
+set ERROR_CODE=%ERRORLEVEL%
+goto finally
+
+:export_cfe
+
+echo [INFO] Loading configuration extension from file "%V8_SRC_PATH%" to infobase "%IB_PATH%"...
+
+IF "%V8_CONVERT_TOOL%" equ "designer" (
+    set V8_DESIGNER_LOG=%LOCAL_TEMP%\v8_designer_output.log
+    %V8_TOOL% DESIGNER /IBConnectionString %V8_IB_CONNECTION% /N"%V8_IB_USER%" /P"%V8_IB_PWD%" /DisableStartupDialogs /Out "!V8_DESIGNER_LOG!" /LoadCfg "%V8_SRC_PATH%" -Extension %V8_EXT_NAME%
+    FOR /F "tokens=* delims=" %%i IN (!V8_DESIGNER_LOG!) DO IF "%%i" neq "" echo [WARN] %%i
+) ELSE (
+    IF defined V8_IB_SERVER (
+        %IBCMD_TOOL% infobase config load --data="%IBCMD_DATA%" --dbms=%V8_DB_SRV_DBMS% --db-server=%V8_IB_SERVER% --db-name="%V8_IB_NAME%" --db-user="%V8_DB_SRV_USR%" --db-pwd="%V8_DB_SRV_PWD%" --user="%V8_IB_USER%" --password="%V8_IB_PWD%" --extension=%V8_EXT_NAME% --force "%V8_SRC_PATH%"
+    ) ELSE (
+        %IBCMD_TOOL% infobase config load --data="%IBCMD_DATA%" --db-path="%IB_PATH%" --user="%V8_IB_USER%" --password="%V8_IB_PWD%" --extension=%V8_EXT_NAME% --force "%V8_SRC_PATH%"
+    )
+)
+set ERROR_CODE=%ERRORLEVEL%
+
+:finally
+
+echo [INFO] Clear temporary files...
+IF exist "%LOCAL_TEMP%" rd /S /Q "%LOCAL_TEMP%"
+
+exit /b %ERROR_CODE%
