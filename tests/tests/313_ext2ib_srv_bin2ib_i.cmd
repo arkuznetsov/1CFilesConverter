c45fd603d63365b44f7ac439593e7d8de89c33de
ArKuznetsov <ArKuznetsov@gmail.com>
2024-10-04 16:17:18 +0300

    fix: В тестах добавлена установка пути к утилите RAC.

--- tests/tests/313_ext2ib_srv_bin2ib_i.cmd
+++ tests/tests/313_ext2ib_srv_bin2ib_i.cmd
@@ -5,6 +5,7 @@ set TEST_OUT_PATH=%OUT_PATH%\%~n0
 set TEST_OUT_PATH=%TEST_OUT_PATH: =_%
 set TEST_CHECK_PATH=%TEST_OUT_PATH%\DataProcessors\Расш1_Обработка1.xml
 set V8_PATH=C:\Program Files\1cv8\%V8_VERSION%\bin
+set RAC_TOOL="%V8_PATH%\rac.exe"
 set V8_CONVERT_TOOL=ibcmd
 
 echo ===
