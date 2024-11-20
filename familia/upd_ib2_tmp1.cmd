a1876a9a6d7dd0d745a103ef955bbf6d75b2fa06
Artem Kuznetsov <arkuznetsov@gmail.com>
2024-11-20 11:43:08 +0300

    test: Тестовые скрипт и настройки проверки обновления расширений

---
+++ familia/upd_ib2_tmp1.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+set V8_UPDATE_SCRIPT=upd_ib2.cmd
+set V8_UPDATE_SETTINGS=tmp1.env
+set V8_UPDATE_LOG=log_tmp1.txt
+
+call %~dp0%V8_UPDATE_SCRIPT% %~dp0%V8_UPDATE_SETTINGS% > %~dp0%V8_UPDATE_LOG%
