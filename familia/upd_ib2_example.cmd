daa493d09da2640425da456111724d80a6539ac2
Artem Kuznetsov <arkuznetsov@gmail.com>
2025-03-05 15:49:15 +0300

    fix: Установка кодировки в примере скрипта запуска

--- familia/upd_ib2_example.cmd
+++ familia/upd_ib2_example.cmd
@@ -1,9 +1,12 @@
 @ECHO OFF
 
-chcp %V8_ENCODING% > nul
+
+set V8_ENCODING=65001
 
 set V8_UPDATE_SCRIPT=upd_ib2.cmd
 set V8_UPDATE_SETTINGS=upd_ib2.env
 set V8_UPDATE_LOG=log_tmb_ib.txt
+
+chcp %V8_ENCODING% > nul
 
 call %~dp0%V8_UPDATE_SCRIPT% %~dp0%V8_UPDATE_SETTINGS% > %~dp0%V8_UPDATE_LOG%
--- familia/upd_ib2_tmp1.cmd
+++ familia/upd_ib2_tmp1.cmd
@@ -1,9 +1,11 @@
 @ECHO OFF
 
-chcp %V8_ENCODING% > nul
+set V8_ENCODING=65001
 
 set V8_UPDATE_SCRIPT=upd_ib2.cmd
 set V8_UPDATE_SETTINGS=tmp1.env
 set V8_UPDATE_LOG=log_tmp1.txt
+
+chcp %V8_ENCODING% > nul
 
 call %~dp0%V8_UPDATE_SCRIPT% %~dp0%V8_UPDATE_SETTINGS% > %~dp0%V8_UPDATE_LOG%
